d000b6478da1781c780e208798ea620448cab840
Takeshi Kanemoto <tak.kanemoto@gmail.com>
2011-06-19 17:17:35 +0900

    Improved installing

--- install.sh
+++ install.sh
@@ -5,15 +5,22 @@ cwd=`dirname "$0"`
 echo "installing from $cwd"
 cd ~/
 
-rm -rf ~/.mc
+rm -rf .mc
 
-[ -d ~/bin ] && mv ~/bin ~/bin.bu
-[ -d ~/.vim ] && mv ~/.vim ~/.vim.bu
+[ -d bin ] && mv bin bin.bu
+[ -d .vim ] && mv .vim .vim.bu
 
 for i in bin notes.txt .mc .nanorc .virc .vim .toprc .bash_prompt .bash_aliases .gitattributes; do
-    [ ! -h ~/$i ] && ln -v -s $cwd/$i ~/$i  || echo "W: ~/$i exists"
+  if [ ! -e $i ]; then
+    ln -v -s $cwd/$i $i
+  else
+    mv $i $i.bak
+    ln -v -s $cwd/$i $i
+  fi
 done
-# [ ! -e ~/.vimrc ] 		&& ln -s $cwd/.virc ~/.vimrc
+
+[ -e .virc ] && mv .virc .virc.bak
+ln -s $cwd/.virc .vimrc
 
 which sudo 2>/dev/null && sudo cp -R $HOME/home/nanosyntax/nanosyntax/nano/*.nanorc /usr/share/nano/
 
